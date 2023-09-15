695984eaa462beb673d6b960a21d9088d9b20ff9
drhcode <doriancocka@gmail.com>
2023-09-15 16:53:25 +0200

    homepage almost ready, featured and posts created

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from './card.module.css';
+import Link from 'next/link';
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>15.09.2023</span>
+          <span className={styles.category}>Coding</span>
+        </div>
+        <Link href='/'>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati molestiae alias
+            sumenda.
+          </h1>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident labore omnis sit. Modi
+          tempora dolor illum accusamus doloribus possimus, ea, distinctio ipsam cumque commodi enim
+          reiciendis, quia dolorem expedita placeat?
+        </p>
+        <Link href='/'>Read More</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
